1f6c141f9a2a23f08b3e390a2a8efe5269127435
Robert Varga <vargarobert97@gmail.com>
2021-06-20 20:39:17 +0200

    Other files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("We're on home");
+});
+
+app.get('/posts', (req, res) => {
+    res.send("We're on posts");
+});
+
+app.listen(3000);
