fbafbe961a44127ca71845f74f4c0c0f5ad75a52
Robert Varga <vargarobert97@gmail.com>
2021-06-22 19:26:03 +0200

    Added controllers for Category and Shop

---
+++ controllers/categoryController.js
@@ -0,0 +1,59 @@
+const Category = require("../models/Category");
+
+const create_category = (req, res) => {
+  const category = new Category({
+    name: req.body.categoryName,
+    description: req.body.categoryDescription
+  })
+  /* 
+  try {
+      const newCategory = await category.save()
+      res.status(201).json(newCategory)
+  } catch (err){
+      res.status(400).json({ message: err.message})
+  }
+})
+  */
+
+  category
+    .save()
+    .then((result) => {
+      res.json("Category created");
+    })
+    .catch((err) => console.log(err));
+};
+
+const get_all_categories = (req, res) => {
+  Category.find()
+    .then((category) => {
+      res.json(category);
+    })
+    .catch((err) => console.log(err));
+};
+
+/* 
+const get_all_categories = (req, res) => {
+    try {
+        const allCategories = await categoryController.find()
+        res.json(allCategories)
+    } catch (err) {
+        res.status(500).json({ message: err.message})
+    }
+})
+*/
+
+const get_single_category = (req, res) => {
+  const id = req.params.id;
+
+  Category.findById(id)
+    .then((category) => {
+      res.json(category);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = {
+  create_category,
+  get_all_categories,
+  get_single_category,
+};
---
+++ controllers/shopController.js
@@ -0,0 +1,40 @@
+const Shop = require("../models/Shop");
+
+const create_shop = (req, res) => {
+  const shop = new Shop({
+    name: req.body.shopName,
+    address: req.body.shopAddress,
+    city: req.body.shopCity,
+  });
+
+  shop
+    .save()
+    .then((result) => {
+      res.json("Shop created");
+    })
+    .catch((err) => console.log(err));
+};
+
+const get_all_shops = (req, res) => {
+  Shop.find()
+    .then((shops) => {
+      res.json(shops);
+    })
+    .catch((err) => console.log(err));
+};
+
+const get_single_shop = (req, res) => {
+  const id = req.params.id;
+
+  Shop.findById(id)
+    .then((shop) => {
+      res.json(shop);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = {
+  create_shop,
+  get_all_shops,
+  get_single_shop,
+};
