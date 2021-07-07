44a3fdd1f989145c22176c8d61c6ba54a26b8357
Robert Varga <vargarobert97@gmail.com>
2021-07-07 23:39:17 +0200

    Chenged form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
 
-const Form = () => {
+const Form = ({setInputText, shops, setShops, inputText, setStatus}) => {
+  const inputTextHandler = (event) => {
+    //console.log(event.target.value);
+    setInputText(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setShops([
+      ...shops, {text: inputText, completed: false, id: Math.random() * 1000} //Ovde ide schema od shop, category i item
+    ]);
+    setInputText('');
+  };
+
+  const statusHandler = (event) => {
+    setStatus(event.target.value);
+  }
     return(
     <form>
-      <input type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-
-
-          <option value="uncompleted">Ubaci Shop List i Controls</option>
-
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="">Select</option>
+          <option value="shops">Shops</option>
+          <option value="categories">Categories</option>
+          <option value="lists">Lists</option>
         </select>
       </div>
     </form>
