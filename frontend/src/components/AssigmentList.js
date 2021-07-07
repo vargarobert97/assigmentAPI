f48a7170b57439739f528466cb78ee9b94ecd2ff
Robert Varga <vargarobert97@gmail.com>
2021-07-07 15:53:15 +0200

    Created Form and Assigment files

---
+++ frontend/src/components/AssigmentList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AssigmentList = () => {
+    return (
+    <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+    );
+}
+
+export default AssigmentList;
