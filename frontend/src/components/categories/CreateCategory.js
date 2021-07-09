2b53d37d1602eeda16960accdf518db1160c1ebd
Robert Varga <vargarobert97@gmail.com>
2021-07-09 20:09:42 +0200

    Added code for Category creation

--- frontend/src/components/categories/CreateCategory.js
+++ frontend/src/components/categories/CreateCategory.js
@@ -1,28 +1,71 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import GetAllCategories from "./GetAllCategories";
 
 const CreateCategory = () => {
-    const [posts, setPosts] = useState([]);
+  //   const [posts, setPosts] = useState([]);
 
-    useEffect(() =>{
-        axios.post('http://localhost:27010/routes/category').then(res => {
-            console.log(res);
-            setPosts(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
+  const categoryNameRef = useRef("");
+  const categoryDescriptionRef = useRef("");
 
-    return(
-        <div>
-            <ul>
-                {
-                    posts.map(post => <li key={post.id}>{post.title}</li>)
-                }
-                </ul>
+  //   useEffect(() => {
+  //     axios
+  //       .post("http://localhost:27010/routes/category")
+  //       .then((res) => {
+  //         console.log(res);
+  //         setPosts(res.data);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }, []);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const category = {
+      categoryName: categoryNameRef.current.value,
+      categoryDescription: categoryDescriptionRef.current.value,
+    };
+
+    console.log(category);
+    axios
+      .post("http://localhost:5500/routes/category", category)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="my-4">
+          <input
+            //   value={inputText}
+            //   onChange={inputTextHandler}
+            placeholder="Category name"
+            type="text"
+            className="todo-input"
+            ref={categoryNameRef}
+          />
         </div>
-    );
-}
 
-export default CreateCategory
+        <div className="my-4">
+          <input
+            placeholder="Category Description"
+            className="todo-input"
+            ref={categoryDescriptionRef}
+          />
+        </div>
+        <button className="my-4" type="submit">
+          Add
+        </button>
+      </form>
+      <GetAllCategories />
+    </div>
+  );
+};
+
+export default CreateCategory;
