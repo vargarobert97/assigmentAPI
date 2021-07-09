46f1d1eae0988250dedbef9fe4b351cf229632db
Robert Varga <vargarobert97@gmail.com>
2021-07-09 15:21:51 +0200

    Fixed axios for Getting all shops

--- frontend/src/components/shops/GetAllShops.js
+++ frontend/src/components/shops/GetAllShops.js
@@ -1,28 +1,43 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Shop from "./Shop";
 
 const GetAllShops = () => {
-    const [posts, setPosts] = useState([]);
+  const [shops, setShops] = useState([]);
 
-    useEffect(() =>{
-        axios.get('http://localhost:3001/routes/shops').then(res => {
-            console.log(res);
-            setPosts(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:5500/routes/shops")
+      .then((res) => {
+        console.log(res);
+        setShops(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return(
-        <div>
-            <ul>
-                {
-                    posts.map(post => <li key={post.id}>{post.title}</li>)
-                }
-                </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Shop Name</th>
+            <th scope="col">Shop City</th>
+            <th scope="col">Shop Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            // posts.map(post => <li key={post._id}>{post.title}</li>)
+            shops.map((shop) => (
+              <Shop key={shop._id} shop={shop} />
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default GetAllShops
+export default GetAllShops;
