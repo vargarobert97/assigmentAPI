d7f5b7fc4dec7ffe53523100dd4863b11b087e33
Robert Varga <vargarobert97@gmail.com>
2021-07-09 15:19:01 +0200

    edited form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from "react";
 
-
-const Form = ({setInputText, shops, setShops, inputText, setStatus}) => {
+const Form = ({ setInputText, shops, setShops, inputText, setStatus }) => {
   const inputTextHandler = (event) => {
     //console.log(event.target.value);
     setInputText(event.target.value);
@@ -9,18 +8,23 @@ const Form = ({setInputText, shops, setShops, inputText, setStatus}) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    setShops([
-      ...shops, {text: inputText, completed: false, id: Math.random() * 1000} //Ovde ide schema od shop, category i item
-    ]);
-    setInputText('');
+    // setShops([
+    //   ...shops, {text: inputText, completed: false, id: Math.random() * 1000} //Ovde ide schema od shop, category i item
+    // ]);
+    // setInputText('');
   };
 
   const statusHandler = (event) => {
     setStatus(event.target.value);
-  }
-    return(
+  };
+  return (
     <form>
-      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
       <button onClick={submitHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
@@ -33,7 +37,7 @@ const Form = ({setInputText, shops, setShops, inputText, setStatus}) => {
         </select>
       </div>
     </form>
-    );
-}
+  );
+};
 
 export default Form;
