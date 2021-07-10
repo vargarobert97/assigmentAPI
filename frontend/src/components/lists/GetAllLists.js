0168c722752cd7e50dd86790b8673fddcb74f2a2
Robert Varga <vargarobert97@gmail.com>
2021-07-10 20:39:43 +0200

    Changed files for getAll and getSingle lists

--- frontend/src/components/lists/GetAllLists.js
+++ frontend/src/components/lists/GetAllLists.js
@@ -1,28 +1,43 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import List from "./List";
 
 const GetAllLists = () => {
-    const [posts, setPosts] = useState([]);
+  const [lists, setLists] = useState([]);
 
-    useEffect(() =>{
-        axios.get('http://localhost:27010/routes/lists').then(res => {
-            console.log(res);
-            setPosts(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:5500/routes/lists")
+      .then((res) => {
+        console.log(res);
+        setLists(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return(
-        <div>
-            <ul>
-                {
-                    posts.map(post => <li key={post.id}>{post.title}</li>)
-                }
-                </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">List Name</th>
+            <th scope="col">List Shop</th>
+            <th scope="col">List Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            // posts.map(post => <li key={post._id}>{post.title}</li>)
+            lists.map((list) => (
+              <List key={list._id} list={list} />
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default GetAllLists
+export default GetAllLists;
--- frontend/src/components/lists/GetSingleList.js
+++ frontend/src/components/lists/GetSingleList.js
@@ -1,28 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const GetSingleList = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() =>{
-        axios.get('http://localhost:27010/routes/lists/ID').then(res => {
-            console.log(res);
-            setPosts(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:5500/routes/lists/ID")
+      .then((res) => {
+        console.log(res);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return(
-        <div>
-            <ul>
-                {
-                    posts.map(post => <li key={post.id}>{post.title}</li>)
-                }
-                </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default GetSingleList
+export default GetSingleList;
