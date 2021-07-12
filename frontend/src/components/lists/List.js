c289edf0f25719c3c71cc26852cb2846658c4edc
Robert Varga <vargarobert97@gmail.com>
2021-07-12 03:22:16 +0200

    Added deleteList and deleteItem from list

--- frontend/src/components/lists/List.js
+++ frontend/src/components/lists/List.js
@@ -1,21 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
+import Item from "../Item";
 
 const List = (props) => {
   const [lists, setLists] = useState([]);
+
+  const deleteList = () => {
+    axios
+      .delete(`http://localhost:5500/routes/lists/${props.list._id}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteItem = (itemId) => {
+    axios
+      .delete(
+        `http://localhost:5500/routes/lists/${props.list._id}/items/${itemId}`
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <tr>
       <td>{props.list.name}</td>
       <td>{props.list.shop.name}</td>
       <td>
-        {props.list.items.map((item) => (
-          <ul>
-            <li>{item.name}</li>
-          </ul>
-        ))}
+        <ul>
+          {props.list.items.map((item) => (
+            <Item item={item} onDeleteItem={deleteItem} />
+          ))}
+        </ul>
         <NavLink to={`/lists/${props.list._id}/items`}>Add new item</NavLink>
+      </td>
+      <td>
+        <button onClick={deleteList} type="button" class="btn btn-danger">
+          <i class="far fa-trash-alt"></i>
+        </button>
       </td>
     </tr>
   );
